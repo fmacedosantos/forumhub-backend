3fd74ce94d274b7595d8d7c0c61842b642964813
fmacedosantos <felipemacedo986@gmail.com>
2025-11-23 15:00:02 -0300

    Autenticando o usuário obtido no GitHub

--- src/main/java/br/com/forum_hub/controller/LoginGithubController.java
+++ src/main/java/br/com/forum_hub/controller/LoginGithubController.java
@@ -1,9 +1,15 @@
 package br.com.forum_hub.controller;
 
+import br.com.forum_hub.domain.autenticacao.DadosToken;
+import br.com.forum_hub.domain.autenticacao.TokenService;
 import br.com.forum_hub.domain.autenticacao.github.LoginGithubService;
+import br.com.forum_hub.domain.usuario.Usuario;
+import br.com.forum_hub.domain.usuario.UsuarioRepository;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,9 +22,13 @@ import java.net.URI;
 public class LoginGithubController {
 
     private final LoginGithubService loginGithubService;
+    private final UsuarioRepository usuarioRepository;
+    private final TokenService tokenService;
 
-    public LoginGithubController(LoginGithubService loginGithubService) {
+    public LoginGithubController(LoginGithubService loginGithubService, UsuarioRepository usuarioRepository, TokenService tokenService) {
         this.loginGithubService = loginGithubService;
+        this.usuarioRepository = usuarioRepository;
+        this.tokenService = tokenService;
     }
 
     @GetMapping
@@ -32,9 +42,18 @@ public class LoginGithubController {
     }
 
     @GetMapping("/autorizado")
-    public ResponseEntity<String> obterToken(@RequestParam String code){
-        var token = loginGithubService.obterEmail(code);
+    public ResponseEntity<DadosToken> autenticarUsuarioOAuth(@RequestParam String code){
+        var email = loginGithubService.obterEmail(code);
 
-        return ResponseEntity.ok(token);
+        var usuario = usuarioRepository.findByEmailIgnoreCaseAndVerificadoTrueAndAtivoTrue(email)
+                .orElseThrow();
+
+        var authentication = new UsernamePasswordAuthenticationToken(usuario, null);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String tokenAcesso = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        String refreshToken = tokenService.gerarRefreshToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosToken(tokenAcesso, refreshToken));
     }
 }
--- src/main/java/br/com/forum_hub/domain/usuario/UsuarioRepository.java
+++ src/main/java/br/com/forum_hub/domain/usuario/UsuarioRepository.java
@@ -12,4 +12,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByToken(String codigo);
 
     Optional<Usuario> findByNomeUsuarioIgnoreCaseAndVerificadoTrueAndAtivoTrue(String nomeUsuario);
+
+    Optional<Usuario> findByEmailIgnoreCaseAndVerificadoTrueAndAtivoTrue(String email);
 }
