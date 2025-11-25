efc744c5b86b0eac0e2f2c3ee5859f540520e369
fmacedosantos <felipemacedo986@gmail.com>
2025-11-24 21:46:04 -0300

    Login com o Google - obtendo token

---
+++ src/main/java/br/com/forum_hub/domain/autenticacao/google/LoginGoogleService.java
@@ -0,0 +1,81 @@
+package br.com.forum_hub.domain.autenticacao.google;
+
+import br.com.forum_hub.domain.autenticacao.github.DadosEmail;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
+
+@Service
+public class LoginGoogleService {
+
+    @Value("${google.oauth.clienteId}")
+    private String clientId;
+    @Value("${google.oauth.clientSecret}")
+    private String clientSecret;
+    private final String redirectUri = "http://localhost:8080/login/google/autorizado";
+    private final RestClient restClient;
+
+    public LoginGoogleService(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.build();
+    }
+
+    public String gerarUrl(){
+        return String.format("https://accounts.google.com/o/oauth2/v2/auth" +
+                "?client_id=%s" +
+                "&redirect_uri=%s" +
+                "&scope=https://www.googleapis.com/auth/userinfo.email" +
+                "&response_type=code", clientId, redirectUri);
+
+    }
+
+    private String obterToken(String code) {
+        var resposta = restClient.post()
+                .uri("https://oauth2.googleapis.com/token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Map.of(
+                        "code", code,
+                        "client_id", clientId,
+                        "client_secret", clientSecret,
+                        "redirect_uri", redirectUri,
+                        "grant_type", "authorization_code"
+                )).retrieve()
+                .body(Map.class);
+
+        return resposta.get("access_token").toString();
+    }
+
+    public String obterEmail(String code){
+        var token = obterToken(code);
+
+        var headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+
+        var resposta = restClient.get()
+                .uri("https://api.github.com/user/emails")
+                .headers(httpHeaders -> httpHeaders.addAll(headers))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(DadosEmail[].class);
+
+        var repositorios = restClient.get()
+                .uri("https://api.github.com/user/repos")
+                .headers(httpHeaders -> httpHeaders.addAll(headers))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(String.class);
+        System.out.println(repositorios);
+
+        for(DadosEmail dadosEmail : resposta){
+            if(dadosEmail.primary() && dadosEmail.verified()) {
+                return dadosEmail.email();
+            }
+        }
+
+        return null;
+    }
+}
